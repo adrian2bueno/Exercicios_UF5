d391ea4871492ead57acea6158da65eab3539b33
adrianbueno <a.bueno2@sapalomera.cat>
2024-03-22 10:07:29 +0100

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) {
+        exercicio1();
+    }
+
+    public static void exercicio1() {
+        
+    }
+}
